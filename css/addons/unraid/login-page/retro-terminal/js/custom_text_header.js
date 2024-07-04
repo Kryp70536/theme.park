ce782700f0c8fdecf1cc346220d91fe7415abe0a
Kryp70536 <kryp70536@outlook.com>
2024-07-04 09:48:07 -0500

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,8 +6,7 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>
-    __                         ___          
+<div class="custom-text-header"> <pre>     __                         ___          
    / /__  ____  ____  __  __  / (_)   _____ 
   / / _ \\/ __ \\/ __ \\/ / / / / / / | / / _ \\
  / /  __/ / / / / / / /_/ / / / /| |/ /  __/,&lt;
